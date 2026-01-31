28f235e584b36aed44a4dc318cbe314e205e9b27
Amy Huang <amyxjhuang@berkeley.edu>
2026-01-31 18:44:01 -0500

    Implement 2 scaling plates

---
+++ Assets/Scripts/Scene 2/ScalingPlateScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ScalingPlateScript : MonoBehaviour
+{
+    [SerializeField] private float scaleSpeed = 1f;
+    [SerializeField] private float scaleAmount = 0.5f;
+    [SerializeField] private Vector3 baseScale = Vector3.one;
+
+    private void Start()
+    {
+        // Store the initial scale as the base scale
+        if (baseScale == Vector3.one)
+        {
+            baseScale = transform.localScale;
+        }
+        scaleAmount = Random.Range(0.3f, 0.7f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the scale factor using sine wave for smooth oscillation
+        float scaleFactor = 1f + Mathf.Sin(Time.time * scaleSpeed) * scaleAmount;
+        
+        transform.localScale = new Vector3(
+            baseScale.x * scaleFactor,
+            baseScale.y,
+            baseScale.z * scaleFactor
+        );
+    }
+}
